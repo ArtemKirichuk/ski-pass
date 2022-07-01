f1cc3c756f36581e8aa45540d241922b9512fa66
aefanov <aefanov@t1-consulting.ru>
2022-07-01 16:45:39 +0300

    feat: добавлены компонент регистрации и копонент кнопки, загружены иконки соцсетей, задник и логотип

--- src/app/modules/registration/registration.component.ts
+++ src/app/modules/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-registration',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
 })
 export class RegistrationComponent {
 
+    form: FormGroup;
 
+    PERSONAL_ACCOUNT_TEXT = 'Личный кабинет';
+    SKI_RESRORT_TEXT = 'горнолыжного курорта';
+    NAME = 'Имя';
+    PASSWORD = 'Пароль';
+    LOGIN = 'Войти';
+    REGISTRATION = 'Зарегистрироваться';
+    COPYRIGHT = '(с) 2021. Все права защещины';
+
+    constructor() { 
+        this.form = new FormGroup({
+            login: new FormControl(null, Validators.required),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
