f9c503dc8319d6c27a01eca5675ef99f133584c0
akirichuk <akirichuk@t1-consulting.ru>
2022-07-06 17:39:25 +0300

    feat: форма ски-пасс. fix: backend update fn

--- src/app/modules/ski-passes/ski-passes.module.ts
+++ src/app/modules/ski-passes/ski-passes.module.ts
@@ -7,16 +7,42 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { SkiPassesCardComponent } from './ski-passes-card/ski-passes-card.component';
+import { ButtonAddModule } from '../button-add/button-add.module';
+import { PaginatorModule } from 'src/app/paginator/paginator.module';
+import { SkiPassesFormComponent } from './form/form.component';
+import { ButtonModule } from '../button/button.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
     declarations: [
         SkiPassesComponent,
-        SkiPassesCardComponent
+        SkiPassesCardComponent,
+        SkiPassesFormComponent
     ],
     imports: [
+        FormsModule,
+        ReactiveFormsModule,
         CommonModule,
+        RouterModule.forChild([{ path: '', component: SkiPassesComponent }]),
+        ButtonAddModule,
+        PaginatorModule,
+        ButtonModule,
+        //mat
+        MatDialogModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        
         MatMenuModule,
         MatIconModule,
-        RouterModule.forChild([{ path: '', component: SkiPassesComponent }])
-    ]
+        MatFormFieldModule,
+    MatInputModule,
+
+    ],
+
 })
 export class SkiPassesModule { }
