192685043ca095aa627af9356d2a017146567372
akirichuk <akirichuk@t1-consulting.ru>
2022-07-07 11:23:34 +0300

    fix: form skipass

--- src/app/modules/ski-passes/form/form.component.ts
+++ src/app/modules/ski-passes/form/form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { i18n, parseImg } from 'src/app/types/helper';
 import { SkiPassType } from 'src/app/types/types';
@@ -10,24 +11,27 @@ import { SkiPassType } from 'src/app/types/types';
   styleUrls: ['./form.component.scss']
 })
 export class SkiPassesFormComponent extends i18n implements OnInit {
-  skiPssForm = new FormGroup({
-    cardNumber: new FormControl(null, [Validators.required]),
-    dateStart: new FormControl(null, [Validators.required]),
-    dateEnd: new FormControl(null, [Validators.required]),
-    cost: new FormControl(null, [Validators.required]),
-    photo: new FormControl('')
-  });
-  srcPhoto:string|SafeUrl = 'assets/images/default-photo.svg'
-  skiPass!: SkiPassType;
+  skiPassForm: FormGroup;
+  srcPhoto: string | SafeUrl = 'assets/images/default-photo.svg'
 
-  constructor(private sanitizer:DomSanitizer){
+  isCreate: boolean;
+  constructor(private sanitizer: DomSanitizer,
+    @Inject(MAT_DIALOG_DATA) public skiPass: SkiPassType) {
     super()
+    this.isCreate = skiPass ? false : true;
+    // this.skiPass.photo = 'assets/images/default-photo.svg'
+    this.skiPassForm = new FormGroup({
+      cardNumber: new FormControl(this.skiPass?.cardNumber, [Validators.required]),
+      dateStart: new FormControl(this.skiPass?.dateStart, [Validators.required]),
+      dateEnd: new FormControl(this.skiPass?.dateEnd, [Validators.required]),
+      cost: new FormControl(this.skiPass?.cost, [Validators.required]),
+      photo: new FormControl(this.skiPass?.photo)
+    });
   }
   ngOnInit(): void {
-  }
-  doneEditProfile() {
 
   }
+
   changeDate() {
 
   }
@@ -35,22 +39,23 @@ export class SkiPassesFormComponent extends i18n implements OnInit {
     const day = (d || new Date());
     return day !== null && day.getTime() >= new Date().setHours(0, 0, 0, 0);
   };
-    //Сохранить изменения
-    saveRow(): void {
-      Object.assign(this.skiPass, this.skiPssForm.value);
-    }
+  //Сохранить изменения
+  saveRow(): void {
+
+    Object.assign(this.skiPass, this.skiPassForm.value);
+  }
   loadPhoto(): void {
     const input = document.createElement('input');
     input.type = 'file';
     input.accept = '.jpg, .jpeg, .png';
-    input.onchange = (event:Event) => {
+    input.onchange = (event: Event) => {
       let file: File = (event.target as HTMLInputElement).files![0]
       parseImg(file, 'URL', (res: string, file: File) => {
-        
+
         // this.imgSrc = res ;
-        
-        this.srcPhoto = this.sanitizer.bypassSecurityTrustUrl(res); 
-        this.skiPssForm.value.photo = res;
+
+        this.srcPhoto = this.sanitizer.bypassSecurityTrustUrl(res);
+        this.skiPassForm.value.photo = res;
       })
     };
     input.click();
