33c86d7ce0352e01430a2d5a01d20d8e43fb81b1
akirichuk <akirichuk@t1-consulting.ru>
2022-07-07 11:26:28 +0300

    fix: form skipass

--- src/app/modules/ski-passes/form/form.component.ts
+++ src/app/modules/ski-passes/form/form.component.ts
@@ -11,7 +11,6 @@ import { SkiPassType } from 'src/app/types/types';
     styleUrls: ['./form.component.scss']
 })
 export class SkiPassesFormComponent extends i18n implements OnInit {
-<<<<<<< HEAD
   skiPassForm: FormGroup;
   srcPhoto: string | SafeUrl = 'assets/images/default-photo.svg'
 
@@ -61,52 +60,5 @@ export class SkiPassesFormComponent extends i18n implements OnInit {
     };
     input.click();
   }
-=======
-    skiPssForm = new FormGroup({
-        cardNumber: new FormControl(null, [Validators.required]),
-        dateStart: new FormControl(null, [Validators.required]),
-        dateEnd: new FormControl(null, [Validators.required]),
-        cost: new FormControl(null, [Validators.required]),
-        photo: new FormControl('')
-    });
-    srcPhoto:string|SafeUrl = 'assets/images/default-photo.svg';
-    skiPass!: SkiPassType;
-
-    constructor(private sanitizer:DomSanitizer){
-        super();
-    }
-    ngOnInit(): void {
-    }
-    doneEditProfile() {
-
-    }
-    changeDate() {
-
-    }
-    compareCompleteDate = (d: Date | null): boolean => {
-        const day = (d || new Date());
-        return day !== null && day.getTime() >= new Date().setHours(0, 0, 0, 0);
-    };
-    //Сохранить изменения
-    saveRow(): void {
-        Object.assign(this.skiPass, this.skiPssForm.value);
-    }
-    loadPhoto(): void {
-        const input = document.createElement('input');
-        input.type = 'file';
-        input.accept = '.jpg, .jpeg, .png';
-        input.onchange = (event:Event) => {
-            const file: File = (event.target as HTMLInputElement).files![0];
-            parseImg(file, 'URL', (res: string, file: File) => {
-        
-                // this.imgSrc = res ;
-        
-                this.srcPhoto = this.sanitizer.bypassSecurityTrustUrl(res); 
-                this.skiPssForm.value.photo = res;
-            });
-        };
-        input.click();
-    }
->>>>>>> 9111541e757545647aeec42d364b59dae437162e
 
 }
