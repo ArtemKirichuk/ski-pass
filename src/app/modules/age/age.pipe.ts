1f976b8939d28f881844a1f933b42e8306b2f72a
aefanov <aefanov@t1-consulting.ru>
2022-07-07 15:23:55 +0300

    fix: исправлил age pipe
    теперь возраст в промежутке от 11 до 19 лет отображается правильно

--- src/app/modules/age/age.pipe.ts
+++ src/app/modules/age/age.pipe.ts
@@ -16,17 +16,20 @@ export class AgePipe implements PipeTransform {
             age = age - 1;
         }
 
+        if (age === 0) return "Менее года";
+        if(age >= 11 && age <= 19) return age.toString() + ' лет';
+
         let ageMod = age;
         do {
             ageMod = ageMod % 10;
         } while(ageMod % 10 > 10);
 
         switch(true) {
-        case ageMod === 0: return age.toString() + ' лет';
-        case ageMod === 1: return age.toString() + ' год';
-        case 2 <= ageMod && ageMod <= 4: return age.toString() + ' года';
-        case 5 <= ageMod && ageMod <= 9: return age.toString() + ' лет';
-        default: return '';
+            case ageMod === 0: return age.toString() + ' лет';
+            case ageMod === 1: return age.toString() + ' год';
+            case 2 <= ageMod && ageMod <= 4: return age.toString() + ' года';
+            case 5 <= ageMod && ageMod <= 9: return age.toString() + ' лет';
+            default: return '';
         }
     }
 
